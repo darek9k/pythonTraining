af4a477d04438a50941deb79c2da0ae7c9d7f761
darek <darecki9k@gmail.com>
2024-02-02 19:12:21 +0100

    basic types of variables in Python

---
+++ dataTypes.py
@@ -0,0 +1,16 @@
+# basic types of variables in Python
+
+# integers
+print(1)
+print(10323)
+# floating points numbers
+print(1.234)
+# strings
+print("Hello I am String")
+print('Hello I am String')
+# complex numbers
+print(2+4j)
+# boolean
+print(True)
+print(False)
+
