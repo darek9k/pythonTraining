99d5b67e2d56c9ba05f2edd26cb1639adf3b6fec
darek <darecki9k@gmail.com>
2024-02-02 19:45:03 +0100

    variables - valid names and illegal names

---
+++ converting.py
@@ -0,0 +1,3 @@
+print("1" + str(1))
+print(1 + int('1'))
+print(1 + float('2.4'))
---
+++ variables.py
@@ -0,0 +1,25 @@
+firstNumber = 2
+secondNumber = 3
+result = firstNumber + secondNumber
+print(result)
+
+name = 'Joe'
+print('My names is ' + name)
+
+# variable names
+number = 1
+firstNumber1 = 1
+_number = 1
+NUMBER = 1
+Number = 1
+# case sensitivity
+
+# wrong variable names
+# first-number
+# first number
+# 1_number = 1
+# number$ = 20
+
+
+
+
