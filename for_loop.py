ba8e99a3cf971165d9fc6b5791a37b6a6e7c5e92
darek <darecki9k@gmail.com>
2024-02-05 09:51:40 +0100

    Loops

---
+++ for_loop.py
@@ -0,0 +1,5 @@
+for number in range(10):
+    print(number)
+
+for character in "Computer":
+    print(character)
--- if_statement.py
+++ if_statement.py
@@ -3,4 +3,30 @@ print("I'm second line")
 hungry = False
 if hungry:
     print("I'm hungry, should I eat something?")
-print("I open the fridge")
+    print("I open the fridge")
+else:
+    print("I'm full")
+
+day = "ups"
+
+if day == "Monday":
+    print("Its Monday")
+elif day == "Tuesday":
+    print("Its Tuesday")
+elif day == "Wednesday":
+    print("Wednesday")
+elif day == "Thursday":
+    print("Thursday")
+elif day == "Friday":
+    print("Friday")
+elif day == "Saturday":
+    print("Saturday")
+elif day == "Sunday":
+    print("Sunday")
+else:
+    print("Not defined")
+
+
+
+
+
