b8de1d8b71b3a92efcbfffb46c99d6fbd660c089
darek <darecki9k@gmail.com>
2024-02-08 08:54:28 +0100

    find_element and elements - basics

--- demo_test.py
+++ demo_test.py
@@ -23,5 +23,18 @@ driver.find_element(By.CSS_SELECTOR, "a")
 driver.find_element(By.CSS_SELECTOR, "img#smileImage")
 driver.find_element(By.CSS_SELECTOR, "p.topSecret")
 print(driver.find_element(By.CSS_SELECTOR, "th:first-child").text)
-driver.quit()
 
+
+# xpath
+driver.find_element(By.XPATH, "/html/body/table/tbody/tr/th")
+driver.find_element(By.XPATH, "//tbody//th")
+driver.find_element(By.XPATH, "//th")
+driver.find_element(By.XPATH, "//th[text()='Month']")
+driver.find_element(By.XPATH, "//button[@id='clickOnMe']")
+driver.find_element(By.XPATH, "//input[@id='fname']/following-sibling::table")
+
+# find_elements
+print(len(driver.find_elements(By.XPATH, "//th")))
+
+
+driver.quit()
