52f2455284978f7c7e429a4efaa687d7fdc0b7c7
darek <darecki9k@gmail.com>
2024-02-08 09:23:27 +0100

    handling_alerts

---
+++ handling_alerts.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+driver.get("file:///C:/Users/Master/Desktop/forPythonTests/Test.html")
+# driver.maximize_windows()
+
+driver.find_element(By.ID, "clickOnMe").click()
+driver.switch_to.alert.accept()
+driver.find_element(By.ID, "clickOnMe").click()
+driver.switch_to.alert.dismiss()
