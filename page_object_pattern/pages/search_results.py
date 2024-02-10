6b678a695d00a6266c2810becc4f5938533aa0c6
darek <darecki9k@gmail.com>
2024-02-10 15:27:51 +0100

    loggern

--- page_object_pattern/pages/search_results.py
+++ page_object_pattern/pages/search_results.py
@@ -1,3 +1,5 @@
+import logging
+
 from selenium.webdriver.common.by import By
 
 
@@ -5,13 +7,22 @@ class SearchResultPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(__name__)
         self.hotel_names_xpath = "//h4[contains(@class,'RTL')]//b"
         self.hotel_prices_xpath = "//div[contains(@class,'price_tab')]//b"
 
     def get_hotel_names(self):
         hotels = self.driver.find_elements(By.XPATH, self.hotel_names_xpath)
-        return [hotel.get_attribute("textContent") for hotel in hotels]
+        names = [hotel.get_attribute("textContent") for hotel in hotels]
+        self.logger.info("Available hotels are: ")
+        for name in names:
+            self.logger.info(name)
+        return names
 
     def get_hotel_prices(self):
         prices = self.driver.find_elements(By.XPATH, self.hotel_prices_xpath)
-        return [price.get_attribute("textContent") for price in prices]
+        hotel_prices = [price.get_attribute("textContent") for price in prices]
+        self.logger.info("Prices are: ")
+        for price in hotel_prices:
+            self.logger.info(price)
+        return hotel_prices
