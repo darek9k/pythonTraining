83934df651f70b61d07910228878b4364cc5106a
darek <darecki9k@gmail.com>
2024-02-11 16:39:44 +0100

    DriverFactory - running tests on different browsers

--- page_object_pattern/tests/conftest.py
+++ page_object_pattern/tests/conftest.py
@@ -1,18 +1,14 @@
 import allure
 import pytest
 from allure_commons.types import AttachmentType
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+
+from page_object_pattern.utils.driver_factory import DriverFactory
 
 
 @pytest.fixture()
 def setup(request):
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    driver = DriverFactory.get_driver("chrome")
     driver.implicitly_wait(10)
-    driver.maximize_window()
     # driver field defined
     request.cls.driver = driver
     before_failed = request.session.testsfailed
